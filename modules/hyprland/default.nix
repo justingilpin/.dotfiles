495d252f1bb056df4912eb9b1656e5b8c5b62e4d
Justin Gilpin <justin.lee.gilpin@gmail.com>
2023-04-26 08:38:27 +0800

    added hyprlan and eww modules

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+{ inputs, pkgs, lib, config, ... }:
+
+with lib;
+let cfg = config.modules.hyprland;
+
+in {
+    options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
+    config = mkIf cfg.enable {
+	home.packages = with pkgs; [
+	    wofi swaybg wlsunset wl-clipboard hyprland
+	];
+
+        home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+    };
+=======
 {
   imports = [
  #   ./events.nix
@@ -5,4 +21,5 @@
  #   ./windowrules.nix # includes layerrules
  #   ./animations.nix
   ];
+>>>>>>> 55b7ab876808de6e97fb99da36e72842c136fd73
 }
