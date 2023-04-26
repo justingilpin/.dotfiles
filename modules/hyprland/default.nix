76c11806875ef7a28c45e1bb98a6987e822cf43b
Justin Gilpin <justin.lee.gilpin@gmail.com>
2023-04-26 19:28:29 +0800

    updated hyprland

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -1,25 +1,34 @@
-<<<<<<< HEAD
-{ inputs, pkgs, lib, config, ... }:
+{ inputs, pkgs, lib, config, ... }: {
 
-with lib;
-let cfg = config.modules.hyprland;
+  programs.hyprland.package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
 
-in {
-    options.modules.hyprland= { enable = mkEnableOption "hyprland"; };
-    config = mkIf cfg.enable {
-	home.packages = with pkgs; [
-	    wofi swaybg wlsunset wl-clipboard hyprland
-	];
-
-        home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
-    };
-=======
-{
   imports = [
- #   ./events.nix
- #   ./config.nix
- #   ./windowrules.nix # includes layerrules
- #   ./animations.nix
+
   ];
->>>>>>> 55b7ab876808de6e97fb99da36e72842c136fd73
+
+
+  programs = {
+    # enable hyprland and required options
+    hyprland = {
+      enable = true;
+      nvidiaPatches = true;
+#      xwayland.hidpi = true;
+    };
+
+    # backlight control
+ #   light.enable = true;
+
+ #   steam.enable = true;
+
+#    sway = {
+#      enable = true;
+  #    package = inputs.self.packages.${pkgs.hostPlatform.system}.sway-hidpi;
+    };
+
+    # add hyprland to display manager sessions
+  #  xserver.displayManager.sessionPackages = [inputs.hyprland.packages.${pkgs.hostPlatform.system}.default];
+  
+
+
+
 }
