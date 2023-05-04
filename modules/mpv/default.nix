d6fc20c3cdca18782e4b473e0bb962c2cfb93afb
Justin Gilpin <justin.lee.gilpin@gmail.com>
2023-05-04 09:21:11 +0800

    added fonts to hyprland

--- modules/mpv/default.nix
+++ modules/mpv/default.nix
@@ -1,11 +1,13 @@
 { lib, pkgs, user, ... }:
+{ 
 
-{
-  programs = {
-    mpv = {
-      enable = true;
-    };
+#  programs = {
+#    mpv = {
+#      enable = true;
+#    };
+#  };
+  home-manager.users.justin ={
+    home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
+    home.file.".config/mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
   };
-  home.file.".config/mpv/mpv.conf".source = ./mpv.conf;
-  home.file.".config/mpv/scripts/file-browser.lua".source = ./scripts/file-browser.lua;
 }
