84971202bc585627d261484c753a4728f1c5e617
Justin Gilpin <justin.lee.gilpin@gmail.com>
2023-05-19 23:38:19 +0800

    updated nvim

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -1,31 +1,20 @@
 { config, pkgs, ... }:
 {
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-      packer-nvim
-    ];
-    extraConfig = ''
-      packadd! packer.nvim
-      luafile ${./files/nvim/lua/util.lua}
-      luafile ${./files/nvim/lua/plugins.lua}
-      luafile ${./files/nvim/lua/options.lua}
-      luafile ${./files/nvim/lua/keys.lua}
-    '';
+  programs = {
+    neovim = {
+      enable = true;
+      package = pkgs.neovim-unwrapped;
+      plugins = with pkgs.vimPlugins; [
+    #    packer-nvim
+      ];
+      extraConfig = ''
+    #    packadd! packer.nvim
+    #    luafile ${./lua/util.lua}
+    #    luafile ${./lua/plugins.lua}
+    #    luafile ${./lua/options.lua}
+    #    luafile ${./lua/keys.lua}
+      '';
+    };
   };
-
-  environment.systemPackages = with pkgs; [
-    # nix
-    rnix-lsp
-
-    # rust
-    rustfmt
-
-    # lua
-    efm-langserver
-    luaformatter
-    sumneko-lua-language-server
-
-    nodejs_latest
-  ];
 }
+
