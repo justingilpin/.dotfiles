1a6fbcdf49f6dcc14886cc71f959aae3f29d5a59
Justin Gilpin <justin.lee.gilpin@gmail.com>
2024-01-24 14:49:30 +0800

    added desktop config to flake

--- flake.nix
+++ flake.nix
@@ -41,5 +41,29 @@
         ];
       };
     };
+
+    nixosConfigurations = {
+      # TODO please change the hostname to your own
+      seykota = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+    #    inherit config overlays;
+        modules = [
+          ./hosts/desktop/configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # TODO replace justin with your own username
+            home-manager.users.justin = import ./home/default.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
   };
 }
