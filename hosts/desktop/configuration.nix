56bc90bf6e7928401fced9916da119379c68f19c
Justin Gilpin <justin.lee.gilpin@gmail.com>
2024-02-06 01:50:05 +0800

    updated automount

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -50,9 +50,10 @@
   systemd.services.NetworkManager-wait-online.enable = false; 
   
   # Set your time zone.
-  time.timeZone = "Asia/Manila";
+  time.timeZone = "Asia/Hong_Kong";
   #time.timeZone = "America/New_York";
-
+  #time.hardwareClockInLocalTime = true;
+  services.localtimed.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
