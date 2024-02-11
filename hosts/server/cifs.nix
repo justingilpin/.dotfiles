e04e6fd7785d48afe519abeff3adeb270c0be66a
Justin <justin.lee.gilpin.com>
2024-02-12 07:05:31 +0800

    added cifs

---
+++ hosts/server/cifs.nix
@@ -0,0 +1,93 @@
+{ config, lib, pkgs, ... }:
+
+{
+
+  # mount cifs movies
+    fileSystems."/mnt/movies" = {
+    device = "//192.168.88.156/Alliance/media/movies";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs tvshows
+    fileSystems."/mnt/tvshows" = {
+    device = "//192.168.88.156/Alliance/media/tvshows";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs downloads
+    fileSystems."/mnt/downloads" = {
+    device = "//192.168.88.156/Alliance/downloads";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs music
+    fileSystems."/mnt/music" = {
+    device = "//192.168.88.156/Alliance/media/music";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs audiobooks
+    fileSystems."/mnt/audiobooks" = {
+    device = "//192.168.88.156/Alliance/media/audiobooks";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs podcasts
+    fileSystems."/mnt/podcasts" = {
+    device = "//192.168.88.156/Alliance/media/podcasts";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs books
+    fileSystems."/mnt/books" = {
+    device = "//192.168.88.156/Alliance/media/books";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+  # mount cifs nextcloud
+    fileSystems."/mnt/nextcloud" = {
+    device = "//192.168.88.156/Alliance/nextcloud";
+    fsType = "cifs";
+    options = let
+      # this line prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };
+
+}
--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-#      ./dashy.nix
+      ./cifs.nix
     ];
   
   nixpkgs = {
