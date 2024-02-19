4f01aac53b511ac3206eacbfdd7e356a41db979b
justin <justin.lee.gilpin@gmail.com>
2024-02-19 13:57:12 +0800

    hiding some broken mounts

--- hosts/desktop/mounts.nix
+++ hosts/desktop/mounts.nix
@@ -7,20 +7,20 @@
       fsType = "ntfs3";
     };
 
-  fileSystems."/mnt/storage" =
-    { device = "/dev/disk/by-uuid/34E2AD0DE2ACD480"; # 34E2AD0DE2ACD480
-      fsType = "ntfs3";
-    };
+#  fileSystems."/mnt/storage" =
+#    { device = "/dev/disk/by-uuid/34E2AD0DE2ACD480"; # 34E2AD0DE2ACD480
+#      fsType = "ntfs3";
+#    };
 
   fileSystems."/mnt/windows" =
     { device = "/dev/disk/by-uuid/8644AF8B44AF7C95"; #8644AF8B44AF7C95
       fsType = "ntfs3";
     };
 
-  fileSystems."/mnt/m2storage" =
-    { device = "/dev/disk/by-uuid/58BE882EBE88072A"; # 58BE882EBE88072A      
-      fsType = "ntfs3";
-    };
+#  fileSystems."/mnt/m2storage" =
+#    { device = "/dev/disk/by-uuid/58BE882EBE88072A"; # 58BE882EBE88072A      
+#      fsType = "ntfs3";
+#    };
 
   # mount cifs truenas scale need cifs-utils package
     fileSystems."/mnt/alliance" = {
