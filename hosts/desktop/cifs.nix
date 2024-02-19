6cae6b69022bda1db6ab073dc874b1e0bc92c36c
justin <justin.lee.gilpin@gmail.com>
2024-02-19 13:37:25 +0800

    updating cifs for desktop

--- hosts/desktop/cifs.nix
+++ hosts/desktop/cifs.nix
@@ -2,22 +2,22 @@
 {
 
   fileSystems."/mnt/ssdstorage" =
-    { device = "/dev/disk/by-uuid/887C874A7C8731CC";
+    { device = "/dev/disk/by-uuid/887C874A7C8731CC"; #887C874A7C8731CC
       fsType = "ntfs3";
     };
 
   fileSystems."/mnt/storage" =
-    { device = "/dev/disk/by-uuid/34E2AD0DE2ACD480";
+    { device = "/dev/disk/by-uuid/34E2AD0DE2ACD480"; # 34E2AD0DE2ACD480
       fsType = "ntfs3";
     };
 
   fileSystems."/mnt/windows" =
-    { device = "/dev/disk/by-uuid/8644AF8B44AF7C95";
+    { device = "/dev/disk/by-uuid/8644AF8B44AF7C95"; #8644AF8B44AF7C95
       fsType = "ntfs3";
     };
 
   fileSystems."/mnt/m2storage" =
-    { device = "/dev/disk/by-uuid/027E11627E115031";
+    { device = "/dev/disk/by-uuid/58BE882EBE88072A"; # 58BE882EBE88072A      
       fsType = "ntfs3";
     };
 
