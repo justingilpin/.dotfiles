4a116c37b2bba2a282c182c029e45ba4ed323646
Justin <justin.lee.gilpin.com>
2024-03-02 07:16:32 +0800

    adding laptop hyprland

--- home/default.nix
+++ home/default.nix
@@ -3,51 +3,16 @@
   home.username = "justin";
   home.homeDirectory = "/home/justin";
   home.stateVersion = "23.05";
-
   imports = [
-    #./gtk
-    #./desktop
-    #./devel
-    #./media
-    #nix-colors.homeManagerModule
    ./../modules/nvim
-#   ./../modules/plasma
-#  ./../modules/nvim-ide
   ];
 
-  # colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
 
   programs.home-manager.enable = true;
-  home.file.".background-image".source = ./images/oceansbg.jpg;
-#  programs.vscode = {
-#  enable = true;
-#  extensions = with pkgs.vscode-extensions; [
-#    dracula-theme.theme-dracula
-#    vscodevim.vim
-#    yzhang.markdown-all-in-one
-#  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-#    {
-#      name = "remote-ssh-edit";
-#      publisher = "ms-vscode-remote";
-#      version = "0.47.2";
-#      sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
-#    }
-#  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-#    {
-#      name = "signageos-vscode-sops-beta";
-#      publisher = "signageos";
-#      version = "0.7.0";
-#      sha256 = "w7v4rAd9LN1Hd2WIF4W1TcHPUcxsoIiHCYe4QNIjHvw=";
-#    }
-#  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-#    {
-#      name = "joplin-vscode-plugin";
-#      publisher = "rxliuli";
-#      version = "1.1.1";
-#      sha256 = "77U9xu3MNw1Ao6MXOYiqemr/9hevMN7Wm70Tur9HRP4=";
-#    }
-#  ];
-#  };
+
+  home.file."~/.config/hypr/hyprland.config".source = ~/.dotfiles/home/files/hyprland.config;
+  home.file."~/.config/hypr/hyprpaper.config".source = ~/.dotfiles/home/files/hyprpaper.conig;
+
   programs.zsh = { 
       enable = true;
       enableCompletion = true;
@@ -72,22 +37,7 @@
     yzhang.markdown-all-in-one
   ];
 };
- # programs.neovim = {
- #   enable = true;
- #   package = pkgs.neovim-unwrapped;
- #   plugins = with pkgs.vimPlugins; [
- #      nvim-surround
- #      packer-nvim
- #   ];
- #   extraConfig = ''
- #     packadd! packer.nvim
- #     luafile ${./files/nvim/lua/util.lua}
- #     luafile ${./files/nvim/lua/plugins.lua}
- #     luafile ${./files/nvim/lua/options.lua}
- #     luafile ${./files/nvim/lua/keys.lua}
- #   '';
- # };
-#  home.file.".config/nvim/settings.lua".source = ./files/nvim/lua/init.lua;
+
   home.packages = with pkgs; [
 #     texlive.combined.scheme-full
 #        auctex
@@ -106,26 +56,13 @@
           httpx
           pygobject3
         ]))
-          # Wine + winetricks
-#          wineWowPackages.waylandFull
-#          winetricks
-          # Vulkan
-#          vulkan-cts
-#          vulkan-tools
 
 
         #-------Favorite Software-------#
  #       gimp-with-plugins
- #       deluge-gtk
         lf # file manager | ranger replacement
- #       gperftools # TCMalloc for development 
         neofetch
- #       mpd-small
         discord
- #       pulseaudio
- #       pavucontrol
- #       openvpn
- #       xfce.thunar # file manager
  #       sioyek # pdf viewer
         libreoffice-still
         obsidian
@@ -137,7 +74,6 @@
 	tor-browser
 	ffmpeg-headless # codex for video streaming
 
-#        restic
         #------------ZSH----------------#
         starship
         zsh-syntax-highlighting
@@ -149,63 +85,6 @@
  #       sops
  #       age
 
- #       nodePackages.pyright
- #       nodejs
- #       nodePackages.npm
- #       nodePackages.tailwindcss
- #       nodePackages.postcss-cli
- #       nodePackages.typescript
- #       nodePackages.degit
- #       nodePackages.postcss
- #       rustc
- #       cargo
- #       go
- #       thefuck
- #       jq
- #       pup
-        #libby
- #       gcc
- #       binutils
- #       (ripgrep.override { withPCRE2 = true; })
- #       gnutls
- #       fd
- #       imagemagick
- #       zstd
- #       nodePackages.javascript-typescript-langserver
- #       sqlite
- #       editorconfig-core-c
- #       emacs-all-the-icons-fonts
- #       hugo
- #       nix-prefetch-git
     ];
     
-#    nixpkgs.overlays = [
-#      (
-#        self: super: {
-#          libby = super.callPackage ../packages/libby/default.nix {};
-#        }
-#      ) 
-#    ];
-
-#    xresources = {
-#      extraConfig = ''
-#        ! ${config.colorScheme.slug} !
-        #define bg #${config.colorScheme.colors.base00}
-        #define bgBright #${config.colorScheme.colors.base02}
-        #define fg #${config.colorScheme.colors.base06}
-        #define red #${config.colorScheme.colors.base08}
-        #define orange #${config.colorScheme.colors.base09}
-        #define yellow #${config.colorScheme.colors.base0A}
-        #define green #${config.colorScheme.colors.base0B}
-        #define teal #${config.colorScheme.colors.base0C}
-        #define blue #${config.colorScheme.colors.base0D}
-        #define purple #${config.colorScheme.colors.base0E}
-        #define brown #${config.colorScheme.colors.base0F}
-#      '';
-#      properties = {
-#        "*.background" = "bg";
-#        "*.foreground" = "fg";
-#        "*.dpi" = 100; # default 120
-#      };
-#    };
 }
